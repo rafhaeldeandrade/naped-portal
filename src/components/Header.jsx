323660d136e26e5713b255369e122d5a65778ff6
= <=>
2022-03-04 13:27:18 -0300

    refactoring: header component - margins, font-styles and color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,35 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
-import styles from './Header.css';
+import styles from './Header.module.css';
 
 function Header() {
   return (
-    <header className={styles.mainHeader}>
-      <h2 className={styles.napedTitle}>
-        <a href="/">Naped</a>
-      </h2>
-      <nav>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/">Séries</a>
-          </li>
-          <li>
-            <a href="/">Filmes</a>
-          </li>
-          <li>
-            <a href="/">Animes</a>
-          </li>
-          <li>
-            <a href="/">Games</a>
-          </li>
-        </ul>
-      </nav>
-      <button type="button" className="btn-my-account">Minha Conta</button>
+    <header>
+      <a href="/">Naped</a>
+      <div className={styles.rightSideWrapper}>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">Séries</a>
+            </li>
+            <li>
+              <a href="/">Filmes</a>
+            </li>
+            <li>
+              <a href="/">Animes</a>
+            </li>
+            <li>
+              <a href="/">Games</a>
+            </li>
+          </ul>
+        </nav>
+        <button type="button" className={styles.btnMyAccount}>
+          Minha Conta
+        </button>
+      </div>
     </header>
   );
 }
